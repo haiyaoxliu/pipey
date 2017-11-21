dbd8dcf42a53cd77b3d929d8a0c9c40673fc1c38
hliu3 <hliu3@stuy.edu>
2017-11-21 08:46:08 -0500

    p

--- pipey.c
+++ pipey.c
@@ -1,59 +1,50 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char *argv[]){ 
-  
-  int pid; 
-  int p[2];   /* pipe "p" */
-  int q[2];   /* pipe "q" */
-  int a; 
-  int b; 
-  
-  /* Create Pipe. Pipe P is used to transfer information  
-     from the parent process to the child process */
-  a = pipe(p);
-  if(a == -1) 
-    { 
-      fprintf(stderr, "Pipe Failed.\n"); 
-      return EXIT_FAILURE; 
-    } 
-          
-  /* Create second pipe. Pipe Q is used to transfer information 
-     from the child process to the parent process. */
-  b = pipe(q); 
-  if(b == -1) 
-    { 
-      fprintf(stderr, "Pipe Failed.\n"); 
-      return EXIT_FAILURE; 
-    } 
-          
-  /* Create child process */
-  pid = fork(); 
-  
-  switch(pid){ 
-          
-  case -1: /* fork failed */
-    perror("main: fork"); 
-    exit(1); 
-                 
-    /* Child process will execute a loop, waiting for command
-        from the parent process. Child executes the command. Child
-        returns a response to the parent */
-  case 0: /* Child process */
-    printf("Child process ID: %d\n", pid);
-    break;
-    /* do some things */
-  
-    /* Parent process will execute a loop, asking user for a one
-        line command. Parent sends command to child for execution.
-        Parent waits for the response of the child. Parent finally
-        reports the result (displayed on screen). */
-  default: /* Parent process */
-    printf("Parent process ID: %d\n", pid);
-    break;
-    /* do some things */
-  } 
-  getchar();
-  return 0; 
+int main() {
+  int WRITE = 1;
+  int READ = 0;
+  int f, r;
+  int p1[2];
+  int p2[2];
+
+  pipe(p1);
+  pipe(p2);
+  srand(time(NULL));
+  f = fork();
+
+
+  if(f) {
+
+    close(p1[READ]);
+    close(p2[WRITE]);
+
+    r = rand() % 10;
+    write(p1[WRITE], &r, sizeof(int));
+
+    printf("[parent] sending: %d\n", r);
+
+    read(p2[READ], &r, sizeof(int));
+    printf("[parent] received: %d\n", r);
+    
+  }
+
+  if(!f) {
+
+    close(p1[WRITE]);
+    close(p2[READ]);
+
+    read(p1[READ], &r, sizeof(int));
+
+    printf("[child] doing maths on: %d\n", r);
+    r = (r * rand()) % 10;
+
+    write(p2[WRITE], &r, sizeof(int));
+    
+  }
+
+
+  return 0;
 }
